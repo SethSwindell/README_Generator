2e50f5a81f4bfd08fd576331cd200da11dc686d5
Seth Swindell <120345456+SethSwindell@users.noreply.github.com>
2023-03-17 14:03:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,83 +1,83 @@
-const fs = require("fs");
-const util = require("util");
+const fs = require("fs/promises");
 const inquirer = require("inquirer");
-const generateReadme = require("./utils/generateReadme")
-const writeFileAsync = util.promisify(fs.writeFile);
+const generateReadme = require("./utils/generateReadme");
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "projectTitle",
-            message: "Project title?",
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "Type a brief description of your project: "
-        },
-        {
-            type: "input",
-            name: "installation",
-            message: "Describe the installation process: ",
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "What is this project usage for?"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "Chose the appropriate license for this project: ",
-            choices: [
-                "Apache",
-                "Academic",
-                "GNU",
-                "ISC",
-                "MIT",
-                "Mozilla",
-                "Open"
-            ]
-        },
-        {
-            type: "input",
-            name: "contributing",
-            message: "Who are the contributors of this projects?"
-        },
-        {
-            type: "input",
-            name: "tests",
-            message: "Is there a test included?"
-        },
-        {
-            type: "input",
-            name: "questions",
-            message: "What do I do if I have an issue? "
-        },
-        {
-            type: "input",
-            name: "username",
-            message: "Please enter your GitHub username: "
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter your email: "
-        }
-    ]);
+async function promptUser() {
+  const questions = [
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "Project title?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Type a brief description of your project: ",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Describe the installation process: ",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is this project usage for?",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Chose the appropriate license for this project: ",
+      choices: [
+        "Apache",
+        "Academic",
+        "GNU",
+        "ISC",
+        "MIT",
+        "Mozilla",
+        "Open",
+      ],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Who are the contributors of this projects?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Is there a test included?",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "What do I do if I have an issue? ",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Please enter your GitHub username: ",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your email: ",
+    },
+  ];
+
+  const answers = await inquirer.prompt(questions);
+  return answers;
 }
+
 async function init() {
-    try {
-        // Ask user questions and generate responses
-        const answers = await promptUser();
-        const generateContent = generateReadme(answers);
-        // Write new README.md to dist directory
-        await writeFileAsync('./dist/README.md', generateContent);
-        console.log('✔️  Successfully wrote to README.md');
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const answers = await promptUser();
+    const generateContent = generateReadme(answers);
+    await fs.writeFile("./dist/README.md", generateContent);
+    console.log("✔️  Successfully wrote to README.md");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-init();  
+init();
